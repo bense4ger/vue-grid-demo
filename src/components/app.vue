4a19015798d2ac06bfa42fa4ab0b9b884111c228
Ben Seager <github@benseager.co.uk>
2017-09-27 16:32:34 +0100

    added app vue

---
+++ src/components/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="outer">
+      <div id="header">
+          <h2>Some Things</h2>
+      </div>
+      <div id="inner">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'app',
+  computed : {
+      ...mapGetters([
+          'currentPage'
+      ])
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+
+
--- src/vue-app.js
+++ src/vue-app.js
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies,no-param-reassign,import/no-unresolved,import/extensions,max-len */
 import $ from 'jquery';
+import Vue from 'vue';
 import Vuex from 'vuex';
 import {service} from './service';
+
+import App from './components/app.vue';
 
 const store = new Vuex.Store({
   state: {
@@ -19,12 +22,23 @@ const store = new Vuex.Store({
       state.pageData[d.page] = d.data;
     },
   },
+  getters : {
+    currentPage: state => {
+      return state.currentPage;
+    }
+  },
 });
 
 const init = (data) => {
   console.log('Starting app');
   store.commit('incPage');
   store.commit('addData', { page: 1, data });
+
+  new Vue({
+    el: '#app',
+    render: h => h(App),
+    store,
+  });
 };
 
 $(() => {
