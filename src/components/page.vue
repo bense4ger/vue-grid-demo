4ac08f9c15876bacc7ccf26ae71817e13c2b5385
Ben Seager <github@benseager.co.uk>
2017-09-28 21:56:23 +0100

    Start of fetching pages

--- src/components/page.vue
+++ src/components/page.vue
@@ -1,12 +1,56 @@
 <template>
-  <div class="page">{{number}}</div>
+  <div :class="cls" :click="fetch">{{number}}</div>
 </template>
 
 <script>
-export default {
-  name: 'Page',
-  props: ['number']
-}
+  import {mapGetters, mapMutations} from 'vuex';
+  import service from '../service';
+
+  export default {
+    name: 'Page',
+    props: ['number'],
+    data() {
+      return {
+        canFetch: false
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'currentPage'
+      ]),
+      cls() {
+        const base = 'page';
+        if (this.currentPage !== this.number) {
+          this.canFetch = true;
+          return `${base} can-fetch`
+        }
+
+        return base;
+      }
+    },
+    methods: {
+      ...mapMutations([
+        'incPage',
+        'decPage',
+        'addDate'
+      ]),
+      fetch(){
+        if (!this.canFetch) return;
+
+        const toFetch = parseInt(this.number) !== 'NaN'
+          ? this.number
+          : this.number === '<<'
+            ? this.number - 3
+            : this.number + 3;
+
+        service.getPageData(toFetch)
+          .then((data) => {
+
+          })
+          .catch((err) => console.dir(err));
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
